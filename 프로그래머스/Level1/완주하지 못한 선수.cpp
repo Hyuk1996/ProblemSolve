5b339af40127a42c51154f1454ac7dcad9a1b578
eunhyuk <bahngeh@naver.com>
2021-09-17 16:28:21 +0900

    Update 완주하지 못한 선수.cpp

--- 프로그래머스/Level1/완주하지 못한 선수.cpp
+++ 프로그래머스/Level1/완주하지 못한 선수.cpp
@@ -23,3 +23,40 @@ string solution(vector<string> participant, vector<string> completion) {
     
     return answer;
 }
+
+
+// 원래 문제의 의도 (hash 사용)
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    unordered_map<string, int> um;
+    string key;
+    for(int i=0; i<completion.size(); ++i){
+        key = completion[i];
+        if(um.find(key) == um.end())
+            um.insert(make_pair(key, 1));
+        else
+            um[key]++;
+    }
+    
+    for(int i=0; i<participant.size(); ++i){
+        key = participant[i];
+        if(um.find(key) == um.end()){
+            answer = key;
+            break;
+        }
+        else{
+            um[key]--;
+            if(um[key] < 0){
+                answer = key;
+                break;
+            }
+        }
+    }
+    return answer;
+}
