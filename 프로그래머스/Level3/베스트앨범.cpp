bbc95b2972391ed54518d40ee780669f10cc72ec
eunhyuk <bahngeh@naver.com>
2021-10-07 18:10:16 +0900

    programmers 베스트앨범 더 좋은 풀이.

--- 프로그래머스/Level3/베스트앨범.cpp
+++ 프로그래머스/Level3/베스트앨범.cpp
@@ -1,3 +1,4 @@
+// map 이용한 풀이.
 #include <string>
 #include <vector>
 #include <map>
@@ -45,6 +46,52 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
             answer.push_back(v2[j].first);
         }
     }
+    
+    return answer;
+}
+
+// hash 이용한 풀이.
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+bool cmp1(const pair<string, int>& a, const pair<string, int>& b){
+    return a.second > b.second;
+}
+bool cmp2(const pair<int, int>& a, const pair<int, int>& b){
+    if(a.second == b.second)
+        return a.first < b.first;
+    return a.second > b.second;
+}
+vector<int> solution(vector<string> genres, vector<int> plays) {
+    vector<int> answer;
+    
+    unordered_map<string, int> m1; // 장르별 재생 횟수.
+    unordered_map<string, vector<pair<int, int>>> m2; // 장르별 노래 고유번호, 재생 횟수.
+    for(int i=0; i<genres.size(); i++){
+        m1[genres[i]] += plays[i];
+        m2[genres[i]].push_back(make_pair(i, plays[i]));
+    }
+    
+    vector<pair<string, int>> genres_plays(m1.begin(), m1.end());
+    // 많이 재생된 장르 기준으로 정렬.
+    sort(genres_plays.begin(),genres_plays.end(), cmp1);
+    for(int i=0; i<genres_plays.size(); i++){
+        vector<pair<int, int>> v = m2[genres_plays[i].first];
+        // 장르별 재생 횟수 기준으로 정렬.
+        sort(v.begin(), v.end(), cmp2);
+        
+        // 가장 많이 재생된 노래 정답에 삽입.
+        for(int j=0; j<v.size(); j++){
+            if(j >= 2)
+                break;
+            answer.push_back(v[j].first);
+        }
+    }
+    
     
     return answer;
 }
