1ced27ac1500b2a762e0a0f5c0325198fb47a95d
eunhyuk <bahngeh@naver.com>
2021-10-22 15:02:18 +0900

    baekjoon 10816(숫자 카드 2) 정렬 & 이진탐색 이용 풀이.

--- 백준/해쉬/10816(숫자 카드 2).cpp
+++ 백준/해쉬/10816(숫자 카드 2).cpp
@@ -34,3 +34,42 @@ int main(){
     for(int i=0; i<answer.size(); i++)
         cout << answer[i] << ' ';
 }
+
+// 정렬 & 이진탐색 이용한 풀이.
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(){
+    // 입력
+    int N;
+    cin >> N;
+    vector<int> card;
+    for(int i=0; i<N; i++){
+        int tmp;
+        cin >> tmp;
+        card.push_back(tmp);
+    }
+    sort(card.begin(), card.end());
+
+    // 풀이.
+    int M;
+    cin >> M;
+    vector<int> answer;
+    for(int i=0; i<M; i++){
+        int tmp;
+        cin >> tmp;
+        if(lower_bound(card.begin(), card.end(), tmp) == card.end())
+            answer.push_back(0);
+        else{
+            answer.push_back(upper_bound(card.begin(), card.end(), tmp) - lower_bound(card.begin(), card.end(), tmp));
+        }
+    }
+
+    // 출력
+    for(int i=0; i<answer.size(); i++)
+        cout << answer[i] << ' ';
+
+    return 0;
+}
