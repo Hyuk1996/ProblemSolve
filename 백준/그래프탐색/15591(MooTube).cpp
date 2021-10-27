02e70bc7ee1b967c1b401187ae36c37ee9755b5e
eunhyuk <bahngeh@naver.com>
2021-10-27 11:12:07 +0900

    BOJ 15591(MooTube) DFS 이용한 풀이.

--- 백준/그래프탐색/15591(MooTube).cpp
+++ 백준/그래프탐색/15591(MooTube).cpp
@@ -64,3 +64,66 @@ int main(){
         cout << answers[i] << '\n';
     return 0;
 }
+
+
+// DFS 이용 풀이.
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+const int INF = 1000000001;
+
+void DFS(vector<vector<pair<int, int>>>& g, int start, int USADO, vector<int>& USADOs, vector<bool>& visit){
+    visit[start] = true;
+    USADOs[start] = USADO;
+
+    for(int i=0; i<g[start].size(); i++){
+        int next_v = g[start][i].first;
+        int next_USADO = g[start][i].second;
+        if(!visit[next_v]){
+            next_USADO = min(USADO, next_USADO);
+            DFS(g, next_v, next_USADO, USADOs, visit);
+        }
+    }
+}
+int main(){
+    // 입력.
+    int N, Q;
+    cin >> N >> Q;
+    vector<vector<pair<int, int>>> g(N+1, vector<pair<int, int>>());
+    for(int i=0; i<N-1; i++){
+        int p, q, r;
+        cin >> p >> q >> r;
+        g[p].push_back(make_pair(q, r));
+        g[q].push_back(make_pair(p, r));
+    }
+
+    // 동영상 끼리의 USADO 구하기.
+    vector<vector<int>> USADOs(N+1, vector<int>(N+1, INF));
+    for(int i=1; i<=N; i++){
+        vector<bool> visit(N+1, 0);
+        DFS(g, i, INF, USADOs[i], visit);
+    }
+
+    // query 응답.
+    vector<int> answers;
+    while(Q--){
+        int k, v;
+        cin >> k >> v;
+        int ans = 0;
+        for(int i=1; i<=N; i++) {
+            if( i == v)
+                continue;
+            if (k <= USADOs[v][i])
+                ans++;
+        }
+
+        answers.push_back(ans);
+    }
+
+    // 정답 출력.
+    for(int i=0; i<answers.size(); i++)
+        cout << answers[i] << '\n';
+    return 0;
+}
