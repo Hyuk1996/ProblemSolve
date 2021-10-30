60e6b37461592a550b344edbfbd4220df81bc47d
eunhyuk <bahngeh@naver.com>
2021-10-30 16:36:39 +0900

    BOJ 10021(Watering the Fields) Kruskal's 이용 풀이.

--- 백준/MST/10021(Watering the Fields).cpp
+++ 백준/MST/10021(Watering the Fields).cpp
@@ -85,3 +85,100 @@ int main(){
         cout << -1;
     return 0;
 }
+
+// Kruskal's Algorithm 이용.
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+struct DisjointSet{
+    vector<int> parent, rank;
+    // 초기화.
+    DisjointSet(int n) : parent(n), rank(n, 1){
+        for(int i=0; i<n; ++i)
+            parent[i] = i;
+    }
+
+    int find(int u){
+        if(u == parent[u])
+            return u;
+        return parent[u] = find(parent[u]);
+    }
+
+    void merge(int u, int v){
+        u = find(u);
+        v = find(v);
+
+        if(u == v)
+            return;
+        if(rank[u] > rank[v])
+            swap(u, v);
+        parent[u] = v;
+        if(rank[u] == rank[v])
+            ++rank[v];
+    }
+};
+struct edge{
+    int weight;
+    int v;
+    int u;
+};
+
+bool cmp(const edge& a, const edge& b){
+    return a.weight < b.weight;
+}
+int main(){
+    // 입력 받기.
+    int N, C;
+    cin >> N >> C;
+    vector<pair<int, int>> vertices;
+    for(int i=0; i<N; i++){
+        int x, y;
+        cin >> x >> y;
+        vertices.push_back(make_pair(x, y));
+    }
+
+    // 입력 정보 edge list 만들기.
+    vector<edge> edges;
+    for(int i=0; i<vertices.size(); i++){
+        for(int j=i+1; j<vertices.size(); j++){
+            int cost = pow(vertices[i].first - vertices[j].first, 2) + pow(vertices[i].second - vertices[j].second, 2);
+            if(cost >= C){
+                edge e;
+                e.weight = cost;
+                e.v = i;
+                e.u = j;
+                edges.push_back(e);
+            }
+        }
+    }
+    sort(edges.begin(), edges.end(), cmp);
+
+    // Kruskal Algorithm 이용해 MST 구하기.
+    int ans = 0;
+    DisjointSet ds(N);
+    for(int i=0; i<edges.size(); i++){
+        int w = edges[i].weight;
+        int v = edges[i].v;
+        int u = edges[i].u;
+
+        if(ds.find(v) == ds.find(u))
+            continue;
+        ds.merge(u, v);
+        ans += w;
+    }
+
+    // MST 만들 수 있는지 확인.
+    int root = ds.parent[0];
+    for(int i=1; i<N; i++){
+        if(root != ds.find(i)){
+            ans = -1;
+            break;
+        }
+    }
+
+    // 정답 출력.
+    cout << ans;
+}
