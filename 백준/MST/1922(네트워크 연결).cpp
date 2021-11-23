8477992fa6f0763bee68db806787ed41be2b1762
eunhyuk <bahngeh@naver.com>
2021-11-23 12:10:38 +0900

    BOJ 1922(네트워크 연결) Prim's Algorithm 이용한 풀이 추가.

--- 백준/MST/1922(네트워크 연결).cpp
+++ 백준/MST/1922(네트워크 연결).cpp
@@ -66,3 +66,55 @@ int main(){
     cout << total_weight;
     return 0;
 }
+
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+struct cmp{
+    bool operator() (const pair<int, int>& a, const pair<int, int>& b){
+        return a.second > b.second;
+    }
+};
+int main(){
+    // 입력.
+    int N, M;
+    cin >> N >> M;
+    vector<vector<pair<int, int>>> graph(N+1);
+    for(int i=0; i<M; ++i){
+        int a, b, c;
+        cin >> a >> b >> c;
+        graph[a].push_back(make_pair(b, c));
+        graph[b].push_back(make_pair(a, c));
+    }
+
+    // Prim's algorithm 이용해 MST 구하기.
+    vector<bool> visit(N+1, 0);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
+
+    // 시작 정점 1
+    visit[1] = true;
+    for(int i=0; i<graph[1].size(); ++i)
+        pq.push(make_pair(graph[1][i].first, graph[1][i].second));
+
+    int total_weights = 0;
+    while(!pq.empty()){
+        int next_v = pq.top().first;
+        int weight = pq.top().second;
+        pq.pop();
+
+        if(visit[next_v])
+            continue;
+
+        visit[next_v] = true;
+        total_weights += weight;
+        for(int i=0; i<graph[next_v].size(); ++i){
+            if(!visit[graph[next_v][i].first])
+                pq.push(make_pair(graph[next_v][i].first, graph[next_v][i].second));
+        }
+    }
+
+    // 정답 출력.
+    cout << total_weights;
+    return 0;
+}
