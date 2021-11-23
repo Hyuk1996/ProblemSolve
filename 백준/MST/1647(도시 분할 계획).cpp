a01c7461aeaff014ecfb5034aca06cf73c28ce88
eunhyuk <bahngeh@naver.com>
2021-11-23 14:47:57 +0900

    BOJ 1647(도시 분할 계획) 다른 풀이.
    prim's algorithm 이용한 풀이.

--- 백준/MST/1647(도시 분할 계획).cpp
+++ 백준/MST/1647(도시 분할 계획).cpp
@@ -61,3 +61,58 @@ int main(){
     cout << ans;
     return 0;
 }
+
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+struct cmp{
+    bool operator() (const pair<int, int>& a, const pair<int, int>& b){
+        return a.second > b.second;
+    }
+};
+int main(){
+    // 입력.
+    int N, M;
+    cin >> N >> M;
+    vector<vector<pair<int, int>>> graph(N+1);
+    for(int i=0; i<M; ++i){
+        int A, B, C;
+        cin >> A >> B >> C;
+        graph[A].push_back(make_pair(B, C));
+        graph[B].push_back(make_pair(A, C));
+    }
+
+    // Prim's algorithm 이용해 MST 구하기.
+    vector<int> visit(N+1, 0);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
+
+    visit[1] = true;
+    for(int i=0; i<graph[1].size(); ++i)
+        pq.push(make_pair(graph[1][i].first, graph[1][i].second));
+
+    int total_weights = 0;
+    int max_weight = -1;
+    while(!pq.empty()){
+        int v = pq.top().first;
+        int w = pq.top().second;
+        pq.pop();
+
+        if(visit[v])
+            continue;
+        visit[v] = true;
+        total_weights += w;
+        max_weight = max(max_weight, w);
+        for(int i=0; i<graph[v].size(); ++i){
+            if(!visit[graph[v][i].first])
+                pq.push(make_pair(graph[v][i].first, graph[v][i].second));
+        }
+    }
+
+    // 정답 출력.
+    int ans = total_weights - max_weight;
+    cout << ans;
+    return 0;
+}
