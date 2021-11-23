db45f8300b509ab95359f282ef52bcee48731383
eunhyuk <bahngeh@naver.com>
2021-11-23 12:35:18 +0900

    BOJ 1922(네트워크 연결) 다른 풀이.
    Prim's Algorithm을 priority_queue 없이 구현.

--- 백준/MST/1922(네트워크 연결).cpp
+++ 백준/MST/1922(네트워크 연결).cpp
@@ -118,3 +118,62 @@ int main(){
     cout << total_weights;
     return 0;
 }
+
+#include <iostream>
+#include <vector>
+using namespace std;
+const int INF = 10001;
+
+int main(){
+    // 입력.
+    int N, M;
+    cin >> N >> M;
+    vector<vector<pair<int, int>>> graph(N+1);
+    for(int i=0; i<M; ++i){
+        int a, b, c;
+        cin >> a >> b >> c;
+        graph[a].push_back(make_pair(b, c));
+        graph[b].push_back(make_pair(a, c));
+    }
+
+    // Prim's Algorithm 이용해 MST 구하기. 우선순위 큐 없이 구현하기. O(V^2)
+    vector<int> visit(N+1, 0);
+    vector<int> min_weights(N+1, INF);
+
+    visit[1] = true;
+    min_weights[1] = 0;
+    for(int i=0; i<graph[1].size(); ++i){
+        int adj_v = graph[1][i].first;
+        int weight = graph[1][i].second;
+
+        if(min_weights[adj_v] > weight)
+            min_weights[adj_v] = weight;
+    }
+
+    int v_cnt = 1;
+    while(v_cnt < N){
+        int adj_v = 0;
+        for(int i=1; i<=N; ++i){
+            if(!visit[i] && (adj_v == 0 || min_weights[i] < min_weights[adj_v]))
+                adj_v = i;
+        }
+
+        visit[adj_v] = true;
+        for(int i=0; i<graph[adj_v].size(); ++i){
+            int v = graph[adj_v][i].first;
+            int weight = graph[adj_v][i].second;
+
+            if(!visit[v] && min_weights[v] > weight)
+                min_weights[v] = weight;
+        }
+
+        ++v_cnt;
+    }
+
+    // 정답 구하기.
+    int total_weight = 0;
+    for(int i=1; i<=N; i++)
+        total_weight += min_weights[i];
+    cout << total_weight;
+    return 0;
+}
