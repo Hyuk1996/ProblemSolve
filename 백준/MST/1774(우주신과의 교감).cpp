7ad34acf8a690db7370c5529853fa66697bb4986
eunhyuk <bahngeh@naver.com>
2021-11-27 12:38:26 +0900

    BOJ 1774(우주선과의 교감) 다른 풀이.
    Prim's algorithm을 이용한 풀이.

--- 백준/MST/1774(우주신과의 교감).cpp
+++ 백준/MST/1774(우주신과의 교감).cpp
@@ -99,3 +99,93 @@ int main(){
     cout << round(need_cost * 100) / 100;
     return 0;
 }
+
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <queue>
+using namespace std;
+struct cmp{
+    bool operator () (const pair<int, double>& a, const pair<int, double>& b){
+        return a.second > b.second; // 가중치 기준으로 min heap
+    }
+};
+
+double getWeight(const pair<int, int>& v, const pair<int, int>& u){
+    double x_dist = pow((double)v.first - (double)u.first, 2);
+    double y_dist = pow((double)v.second - (double)u.second, 2);
+    return sqrt(x_dist + y_dist);
+}
+int main(){
+    // 입력.
+    int N, M;
+    cin >> N >> M;
+
+    // 정점들의 좌표 정보 저장.
+    vector<pair<int, int>> vertices(N+1);
+    for(int i=1; i<=N; ++i){
+        int X, Y;
+        cin >> X >> Y;
+        vertices[i] = {X, Y};
+    }
+
+    // 정점 좌표정보 이용해 그래프로 표현하기.
+    vector<vector<pair<int, double>>> graph(N+1);
+    for(int i=1; i<=N; ++i){
+        for(int j=i+1; j<=N; ++j){
+            double weight = getWeight(vertices[i], vertices[j]);
+            graph[i].push_back(make_pair(j, weight));
+            graph[j].push_back(make_pair(i, weight));
+        }
+    }
+
+    // 이미 이어진 간선들의 가중치는 0이라고 가정.
+    for(int i=0; i<M; ++i){
+        int v, u;
+        cin >> v >> u;
+        for(int j=0; j<graph[v].size(); ++j) {
+            if (graph[v][j].first == u)
+                graph[v][j].second = 0;
+        }
+        for(int j=0; j<graph[u].size(); ++j){
+            if(graph[u][j].first == v)
+                graph[u][j].second = 0;
+        }
+    }
+
+    // Prim's algorithm 이용해 MST 구하기.
+    vector<bool> visit(N+1, 0);
+    priority_queue<pair<int, double>, vector<pair<int, double>>, cmp> pq;
+
+    visit[1] = true;
+    for(int i=0; i<graph[1].size(); ++i)
+        pq.push(make_pair(graph[1][i].first, graph[1][i].second));
+
+    double need_cost = 0;
+    int vertex_cnt = 1;
+    while(!pq.empty()){
+        int v = pq.top().first;
+        double w = pq.top().second;
+        pq.pop();
+
+        if(visit[v])
+            continue;
+        visit[v] = true;
+        need_cost += w;
+        ++vertex_cnt;
+
+        if(vertex_cnt == N)
+            break;
+        for(int i=0; i<graph[v].size(); ++i){
+            if(!visit[graph[v][i].first])
+                pq.push(make_pair(graph[v][i].first, graph[v][i].second));
+        }
+    }
+
+    // 정답 출력.
+    cout << fixed;
+    cout.precision(2);
+    cout << round(need_cost * 100) / 100;
+    return 0;
+}
