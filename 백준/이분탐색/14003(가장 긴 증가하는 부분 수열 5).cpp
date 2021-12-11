2e5ca66a249a66cbe6038d51bc0e66723f8896ac
eunhyuk <bahngeh@naver.com>
2021-12-11 18:26:29 +0900

    BOJ 14003(가장 긴 증가하는 부분 수열 5) 풀이.
    그리디 패러다임에 이분탐색 알고리즘을 이용해 풀이.

---
+++ 백준/이분탐색/14003(가장 긴 증가하는 부분 수열 5).cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+int main() {
+    // 입력
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i=0; i<n; ++i) {
+        int val;
+        cin >> val;
+        a[i] = val;
+    }
+
+    // 그리디 방식 & 이분 탐색 이용해 최단길이 찾기 , 추가적인 vector 이용해 부분 수열 찾기.
+    int ansIndex = -1;
+    vector<int> answer;
+    vector<int> answerIndex;
+    vector<int> parent(n);
+    for(int i=0; i<n; ++i) {
+        if(answer.empty() || answer.back() < a[i]) {
+            answer.push_back(a[i]);
+            answerIndex.push_back(i);
+            if(answer.size() > 1)
+                parent[i] = answerIndex[answerIndex.size() - 2];
+            else
+                parent[i] = i;
+
+            ansIndex = i;
+        }
+        else {
+            auto it = lower_bound(answer.begin(), answer.end(), a[i]);
+            *it = a[i];
+            int idx = it - answer.begin();
+            answerIndex[idx] = i;
+
+            if(idx > 0)
+                parent[i] = answerIndex[idx - 1];
+            else
+                parent[i] = i;
+        }
+    }
+
+    // 정답 출력.
+    cout << answer.size() << '\n';
+
+    int index = ansIndex;
+    stack<int> s;
+    while(true) {
+        s.push(a[index]);
+        if(parent[index] == index)
+            break;
+        index = parent[index];
+    }
+    while(!s.empty()) {
+        cout << s.top() << ' ';
+        s.pop();
+    }
+    return 0;
+}
