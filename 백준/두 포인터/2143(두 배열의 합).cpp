b15cf8980b80d5a2bf50de4ed82a54021b0112eb
eunhyuk <bahngeh@naver.com>
2022-01-09 17:10:42 +0900

    BOJ 2143(두 배열의 합) 다른 풀이
    첫 번째 풀이는 두 포인터를 이용해 쌍의 개수를 찾았고, 두 번째 풀이는 이분탐색을 이용해 쌍의 개수를 찾았다

--- 백준/두 포인터/2143(두 배열의 합).cpp
+++ 백준/두 포인터/2143(두 배열의 합).cpp
@@ -106,3 +106,97 @@ int main() {
 	cout << answer;
 	return 0;
 }
+
+//이분탐색을 이용한 풀이
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int upperBound(vector<int>& arr, int key) {
+	int lo = 0;
+	int hi = arr.size();
+	while (lo + 1 < hi) {
+		int mid = lo + (hi - lo) / 2;
+		if (arr[mid] <= key) {
+			lo = mid;
+		}
+		else {
+			hi = mid;
+		}
+	}
+
+	if (arr[lo] != key) {
+		return -1;
+	}
+	return lo;
+}
+int lowerBound(vector<int>& arr, int key) {
+	int lo = -1;
+	int hi = arr.size() - 1;
+	while (lo + 1 < hi) {
+		int mid = lo + (hi - lo) / 2;
+		if (arr[mid] >= key) {
+			hi = mid;
+		}
+		else {
+			lo = mid;
+		}
+	}
+
+	if (arr[hi] != key) {
+		return -1;
+	}
+	return hi;
+}
+void getRangeSum(vector<int>& arr, vector<int>& rangeSum) {
+	for (int i = 0; i < arr.size(); ++i) {
+		int sum = 0;
+		for (int j = i; j < arr.size(); ++j) {
+			sum += arr[j];
+			rangeSum.push_back(sum);
+		}
+	}
+}
+long long solve(int& T, int& n, int& m, vector<int>& a, vector<int>& b) {
+	vector<int> rangeSumA;
+	getRangeSum(a, rangeSumA);
+
+	vector<int> rangeSumB;
+	getRangeSum(b, rangeSumB);
+
+	sort(rangeSumA.begin(), rangeSumA.end());
+
+	long long answer = 0;
+	for (int i = 0; i < rangeSumB.size(); ++i) {
+		long long lo = lowerBound(rangeSumA, T - rangeSumB[i]);
+		long long hi = upperBound(rangeSumA, T - rangeSumB[i]);
+
+		if (hi != -1 && lo != -1) {
+			answer += (hi - lo + 1);
+		}
+	}
+	return answer;
+}
+void input(int& T, int& n, int& m, vector<int>& a, vector<int>& b) {
+	cin >> T >> n;
+	a.assign(n, 0);
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i];
+	}
+	cin >> m;
+	b.assign(m, 0);
+	for (int i = 0; i < m; ++i) {
+		cin >> b[i];
+	}
+}
+int main() {
+	int T, n, m;
+	vector<int> a;
+	vector<int> b;
+	input(T, n, m, a, b);
+
+	long long answer = solve(T, n, m, a, b);
+	cout << answer;
+	return 0;
+}
