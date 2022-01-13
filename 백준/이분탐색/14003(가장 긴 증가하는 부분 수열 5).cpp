f973ffbcc266f39e39ed4a88ff72df6673b69545
eunhyuk <bahngeh@naver.com>
2022-01-13 21:45:29 +0900

    BOJ 14003(가장 긴 증가하는 부분 수열 5)풀이
    가장 긴 증가하는 부분 수열을 O(nlogn)에 구하는 알고리즘

--- 백준/이분탐색/14003(가장 긴 증가하는 부분 수열 5).cpp
+++ 백준/이분탐색/14003(가장 긴 증가하는 부분 수열 5).cpp
@@ -1,114 +1,80 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <stack>
 using namespace std;
 
-int main() {
-    // 입력
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for(int i=0; i<n; ++i) {
-        int val;
-        cin >> val;
-        a[i] = val;
-    }
-
-    // 그리디 방식 & 이분 탐색 이용해 최단길이 찾기 , 추가적인 vector 이용해 부분 수열 찾기.
-    int ansIndex = -1;
-    vector<int> answer;
-    vector<int> answerIndex;
-    vector<int> parent(n);
-    for(int i=0; i<n; ++i) {
-        if(answer.empty() || answer.back() < a[i]) {
-            answer.push_back(a[i]);
-            answerIndex.push_back(i);
-            if(answer.size() > 1)
-                parent[i] = answerIndex[answerIndex.size() - 2];
-            else
-                parent[i] = i;
-
-            ansIndex = i;
-        }
-        else {
-            auto it = lower_bound(answer.begin(), answer.end(), a[i]);
-            *it = a[i];
-            int idx = it - answer.begin();
-            answerIndex[idx] = i;
-
-            if(idx > 0)
-                parent[i] = answerIndex[idx - 1];
-            else
-                parent[i] = i;
-        }
-    }
-
-    // 정답 출력.
-    cout << answer.size() << '\n';
-
-    int index = ansIndex;
-    stack<int> s;
-    while(true) {
-        s.push(a[index]);
-        if(parent[index] == index)
-            break;
-        index = parent[index];
-    }
-    while(!s.empty()) {
-        cout << s.top() << ' ';
-        s.pop();
-    }
-    return 0;
+int binarySearch(vector<int>& arr, int key) {
+	int lo = -1;
+	int hi = arr.size() - 1;
+	int mid;
+	while (lo + 1 < hi) {
+		mid = lo + (hi - lo) / 2;
+		if (arr[mid] >= key) {
+			hi = mid;
+		}
+		else {
+			lo = mid;
+		}
+	}
+	return hi;
 }
+void solve(int N, vector<int>& arr, int& LisLen, vector<int>& LIS) {
+	vector<int> v;
+	vector<int> index(N);
+	for (int i = 0; i < N; ++i) {
+		if (v.empty() || v.back() < arr[i]) {
+			v.push_back(arr[i]);
+			index[i] = v.size() - 1;
+		}
+		else {
+			int idx = binarySearch(v, arr[i]); // v 에서 arr[i]보다 같거나 처음으로 큰 수 찾기.
+			v[idx] = arr[i];
+			index[i] = idx;
+		}
+	}
 
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <stack>
-using namespace std;
+	// v의 길이가 LIS
+	LisLen = v.size();
 
+	int tmp = v.size() - 1;
+	stack<int> s;
+	for (int i = N-1; i>=0; --i) {
+		if (index[i] == tmp) {
+			s.push(arr[i]);
+			tmp--;
+			if (tmp == -1) {
+				break;
+			}
+		}
+	}
+
+	while (!s.empty()) {
+		LIS.push_back(s.top());
+		s.pop();
+	}
+}
+void input(int& N, vector<int>& arr) {
+	cin >> N;
+	arr.assign(N, 0);
+	for (int i = 0; i < N; ++i) {
+		cin >> arr[i];
+	}
+}
 int main() {
-    // 입력.
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for(int i=0; i<n; ++i) {
-        int val;
-        cin >> val;
-        a[i] = val;
-    }
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 
-    // 그리디 & 이분탐색 이용해 길이구하고, 추가적인 vector 이용해 부분 수열 구하기.
-    vector<int> LIS;
-    vector<int> index(n);
-    for(int i=0; i<n; ++i) {
-        if(LIS.empty() || LIS.back() < a[i]) {
-            LIS.push_back(a[i]);
-            index[i] = LIS.size() - 1;
-        }
-        else {
-            auto it = lower_bound(LIS.begin(), LIS.end(), a[i]);
-            *it = a[i];
-            index[i] = it - LIS.begin();
-        }
-    }
+	int N;
+	vector<int> arr;
+	input(N, arr);
 
-    // 정답 출력.
-    cout << LIS.size() << '\n';
+	int LisLen;
+	vector<int> LIS;
+	solve(N, arr, LisLen, LIS);
 
-    int tmp = LIS.size() - 1;
-    stack<int> s;
-    for(int i=n-1; i>=0; --i) {
-        if(index[i] == tmp) {
-            s.push(a[i]);
-            --tmp;
-        }
-    }
-
-    while(!s.empty()) {
-        cout << s.top() << ' ';
-        s.pop();
-    }
-    return 0;
+	cout << LisLen << '\n';
+	for (int i = 0; i < LIS.size(); ++i) {
+		cout << LIS[i] << ' ';
+	}
+	return 0;
 }
