ccb86a100bd3ca23ffbc8faf4d9b55bccdce66d5
방은혁 <bahngeh@naver.com>
2022-02-09 09:53:43 +0900

    BOJ 9370(미확인 도착지) 다른 풀이
    기존의 풀이는 다익스트라 한번, 역추적으로 경로 체크.
    이번 풀이는 다익스트라르 3번 돌려서 풀이
    
    포인트
    (목적지가 정점 T 가정)
    1. S -> T (최단거리) == ( S -> G -> GH -> H -> T) or (S -> H -> HG -> G -> T) 이면 S -> T의 최단경로에 GH 간선이 포함된다는 의미

--- 백준/최단경로/9370(미확인 도착지).cpp
+++ 백준/최단경로/9370(미확인 도착지).cpp
@@ -128,3 +128,116 @@ int main() {
     }
     return 0;
 }
+
+//역추적 이용하지 않고 3번의 다익스트라를 활용한 풀이
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+int n, m, t, s, g, h;
+int cost;
+const int INF = 987654321;
+struct cmp {
+    bool operator() (const pair<int, int>& a, const pair<int, int>& b) {
+        return a.second > b.second;
+    }
+};
+
+void dijkstra(vector<vector<pair<int, int>>>& graph, int startV, vector<int>& dists) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
+
+    dists[startV] = 0;
+    pq.push({startV, dists[startV]});
+    while(!pq.empty()) {
+        int curV = pq.top().first;
+        int curDist = pq.top().second;
+        pq.pop();
+
+        if(curDist > dists[curV]) {
+            continue;
+        }
+        for(int i = 0; i < graph[curV].size(); ++i) {
+            int adjV = graph[curV][i].first;
+            int weight = graph[curV][i].second;
+
+            if(dists[curV] + weight < dists[adjV]) {
+                dists[adjV] = dists[curV] + weight;
+                pq.push({adjV, dists[adjV]});
+            }
+        }
+    }
+}
+void solve(vector<vector<pair<int, int>>>& graph, vector<int>& goals, vector<int>& answer) {
+
+    vector<int> distFromS(n + 1, INF);
+    dijkstra(graph, s, distFromS);
+
+    vector<int> distFromG(n + 1, INF);
+    dijkstra(graph, g, distFromG);
+
+    vector<int> distFromH(n + 1, INF);
+    dijkstra(graph, h, distFromH);
+
+    //목적지가 gh 도로를 포함하는지 확인
+    for(int i = 0; i < t; ++i) {
+        int shortest = distFromS[goals[i]];
+        int tmpPath = distFromS[g] + cost + distFromH[goals[i]];
+        int tmpPath2 = distFromS[h] + cost + distFromG[goals[i]];
+
+        if((shortest == tmpPath) || (shortest == tmpPath2)) {
+            answer.push_back(goals[i]);
+        }
+    }
+    sort(answer.begin(), answer.end());
+}
+void input(vector<vector<pair<int, int>>>& graph, vector<int>& goals) {
+    cin >> n >> m >> t;
+    cin >> s >> g >> h;
+
+    graph.assign(n + 1, vector<pair<int, int>>());
+    int a, b, d;
+    for(int i = 0; i < m; ++i) {
+        cin >> a >> b >> d;
+        graph[a].push_back({b, d});
+        graph[b].push_back({a, d});
+
+        if(a == g && b == h) {
+            cost = d;
+        }
+        if(a == h && b == g) {
+            cost = d;
+        }
+    }
+
+    goals.assign(t, 0);
+    for(int i = 0; i < t; ++i) {
+        cin >> goals[i];
+    }
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    vector<vector<int>> answers;
+    int testCase;
+    cin >> testCase;
+    while(testCase--) {
+        vector<vector<pair<int, int>>> graph;
+        vector<int> goals;
+        input(graph, goals);
+
+        vector<int> answer;
+        solve(graph, goals, answer);
+
+        answers.push_back(answer);
+    }
+
+    for(int i = 0; i < answers.size(); ++i) {
+        for(int j = 0; j < answers[i].size(); ++j) {
+            cout << answers[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
