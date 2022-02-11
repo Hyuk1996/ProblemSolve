f7ceed6af8ad13a2d31b4356dc683f2caf8d1c43
방은혁 <bahngeh@naver.com>
2022-02-11 11:49:57 +0900

    BOJ 7578(공장) 풀이
    사용하는 아이디어는 동일
    
    하지만 mapping 정보 구하는 것과, B의 정보를 이용해 IDT를 만든다는게 차이점

--- 백준/트리/7578(공장).cpp
+++ 백준/트리/7578(공장).cpp
@@ -98,3 +98,91 @@ int main() {
 	cout << answer;
 	return 0;
 }
+
+// version 2
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+void updateIDT(vector<int>& IDT, int idx) {
+    idx += (IDT.size() / 2);
+    IDT[idx]++;
+    while(idx > 1) {
+        idx /= 2;
+        IDT[idx] = IDT[idx * 2] + IDT[idx * 2 + 1];
+    }
+}
+int getRangeSum(vector<int>& IDT, int right) {
+    int left = IDT.size() / 2;
+    right += (IDT.size() / 2);
+    int sum = 0;
+    while(left <= right) {
+        if(left % 2 == 1) {
+            sum += IDT[left];
+        }
+        if(right % 2 == 0) {
+            sum += IDT[right];
+        }
+
+        left = (left + 1) / 2;
+        right = (right - 1) / 2;
+    }
+    return sum;
+}
+void makeIDT(vector<int>& IDT, int N) {
+    int leafSize = 1;
+    while(leafSize < N) {
+        leafSize *= 2;
+    }
+
+    IDT.assign(leafSize * 2, 0);
+}
+void getMappingInfo(int N, vector<int>& A, vector<int>& B, vector<int>& map) {
+    unordered_map<int, int> hashMap;
+    for(int i = 0; i < N; ++i) {
+        hashMap[B[i]] = i;
+    }
+
+    for(int i = 0; i < N; ++i) {
+        map[i] = hashMap[A[i]];
+    }
+}
+long long solve(int N, vector<int>& A, vector<int>& B) {
+    //mapping정보 구하기
+    vector<int> map(N, 0);
+    getMappingInfo(N, A, B, map);
+
+    vector<int> IDT;
+    makeIDT(IDT, N);
+
+    long long ans = 0;
+    for(int i = N - 1; i >= 0; --i) {
+        int idx = map[i];
+        ans += getRangeSum(IDT, idx);
+        updateIDT(IDT, idx);
+    }
+    return ans;
+}
+void input(int& N, vector<int>& A, vector<int>& B) {
+    cin >> N;
+    A.assign(N, 0);
+    B.assign(N, 0);
+    for(int i = 0; i < N; ++i) {
+        cin >> A[i];
+    }
+    for(int i = 0; i < N; ++i) {
+        cin >> B[i];
+    }
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N;
+    vector<int> A, B;
+    input(N, A, B);
+
+    cout << solve(N, A, B);
+    return 0;
+}
