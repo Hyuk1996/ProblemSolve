18ea39866ab5d3064051bf79fd7a05ee5eea6231
방은혁 <bahngeh@naver.com>
2022-02-14 09:17:40 +0900

    Programmers 올바른 괄호 풀이
    Stack을 사용하지 않고도 풀이 가능 (Stack을 사용하는 풀이보다 메모리를 효율적으로 사용할 수 있다)

--- 프로그래머스/Level2/올바른 괄호.java
+++ 프로그래머스/Level2/올바른 괄호.java
@@ -24,3 +24,27 @@ class Solution {
         }
     }
 }
+
+//Stack을 사용하지 않는 풀이 (메모리적으로 더 효율적인 풀이)
+class Solution {
+    boolean solution(String s) {
+
+        int count = 0;
+        for(int i = 0; i < s.length(); ++i) {
+            if(s.charAt(i) == '(') {
+                count++;
+            } else {
+                if(count == 0) {
+                    return false;
+                }
+                count--;
+            }
+        }
+        
+        if(count == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
