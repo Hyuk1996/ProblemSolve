b030619a392152f9f69eb6d6bde1cfd11e1d0e1f
방은혁 <bahngeh@naver.com>
2022-03-03 09:23:32 +0900

    Programmers 2 x n 타일링 풀이 v2
    메모리 최적화
    굳이 배열을 이용할 필요 없다. i 번째 값을 구하기 위해 필요한건 i - 1, i -2 값  뿐.

--- 프로그래머스/Level3/2 x n 타일링.java
+++ 프로그래머스/Level3/2 x n 타일링.java
@@ -21,3 +21,28 @@ class Solution {
 
     }
 }
+
+//메모리사용 최적화 풀이
+class Solution {
+    
+    final int CONST_MOD = (int)(1e9 + 7);
+    
+    public int solution(int n) {
+        int answer = 0;
+        
+        int a = 1;
+        int b = 2;
+        if(n == 1) {
+            return a;
+        } else if(n == 2) {
+            return b;
+        } else {
+            for(int i = 3; i <=n; ++i) {
+                answer = (a + b) % CONST_MOD;
+                a = b;
+                b = answer;
+            }
+            return answer;
+        }
+    }
+}
