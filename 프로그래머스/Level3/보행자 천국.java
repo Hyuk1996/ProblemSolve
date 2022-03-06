2d0d0d6f6d3ecdb664ee5a738b58b4dd023189de
방은혁 <bahngeh@naver.com>
2022-03-06 10:49:49 +0900

    Programmers 보행자 천국 다른 풀이.
    첫 번째 풀이와 비교했을때, 2차원 Dp배열을 하나 더 두어, 시간을 절약하는 풀이

--- 프로그래머스/Level3/보행자 천국.java
+++ 프로그래머스/Level3/보행자 천국.java
@@ -56,3 +56,34 @@ class Solution {
         return answer;
     }
 }
+
+// 공간을 많이 사용. 하지만 시간은 절약
+class Solution {
+    int MOD = 20170805;
+    public int solution(int m, int n, int[][] cityMap) {
+        
+        int[][] left = new int[m + 1][n + 1];
+        int[][] up = new int[m + 1][n + 1];
+        left[1][1] = 1;
+        up[1][1] = 1;
+        
+        for(int i = 1; i <= m; ++i) {
+            for(int j = 1; j <= n; ++j) {
+                if(i == 1 && j == 1) {
+                    continue;
+                }
+                
+                if(cityMap[i - 1][j - 1] == 0) {
+                    left[i][j] = (left[i][j - 1] + up[i - 1][j]) % MOD;
+                    up[i][j] = left[i][j];
+                } else if(cityMap[i - 1][j - 1] == 2) { // left[i][j] -> i, j를 왼쪽에서 올 수 있는 경우의 수, up[i][j] -> 위쪽 ~
+                    left[i][j] = left[i][j - 1];
+                    up[i][j] = up[i - 1][j];
+                }
+            }
+        }
+        
+        int answer = left[m][n];
+        return answer;
+    }
+}
