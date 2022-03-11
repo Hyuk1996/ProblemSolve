331cdd862cf5d3fcb064ebbb4e4d2078c8c9ec25
방은혁 <bahngeh@naver.com>
2022-03-11 11:36:10 +0900

    Programmers 광고 삽입 최적화 풀이
    첫 번째 풀이의 최적화 버전
    
    특정 시간의 수청자수를 로그로 일일이 찍는게 아니라, 누적합을 이용해 선형시간에 해결

--- 프로그래머스/Level3/광고 삽입.java
+++ 프로그래머스/Level3/광고 삽입.java
@@ -51,3 +51,48 @@ class Solution {
 }
 
 //최적화 풀이
+class Solution {
+    public String solution(String play_time, String adv_time, String[] logs) {
+        
+        int playT = convertToInt(play_time);
+        int[] viewers = new int[playT + 1];
+        
+        for(String log : logs) {
+            String[] times = log.split("-");
+            viewers[convertToInt(times[0])]++; //시작 시간 시청자 증가
+            viewers[convertToInt(times[1])]--; //끝 시간 시청자 감소
+        }
+        
+        //각 시간에 시청자 수 구하기
+        for(int i = 1; i <= playT; ++i) {
+            viewers[i] += viewers[i - 1];
+        }
+        
+        
+        //누적합 이용해 알맞은 광고 시작시간 구하기
+        int advT = convertToInt(adv_time);
+        long viewerCnt = 0;
+        for(int i = 0; i < advT; ++i) {
+            viewerCnt += viewers[i];
+        }
+        
+        long maxViewerCnt = viewerCnt;
+        int advStartT = 0;
+        for(int i = 1; i <= playT - advT; ++i) {
+            viewerCnt -= viewers[i - 1];
+            viewerCnt += viewers[i + advT - 1];
+            
+            if(viewerCnt > maxViewerCnt) {
+                maxViewerCnt = viewerCnt;
+                advStartT = i;
+            }
+        }
+        
+        return String.format("%02d:%02d:%02d", (advStartT / 3600), (advStartT % 3600) / 60, advStartT % 60);
+    }
+    
+    int convertToInt(String time) {
+        String[] token = time.split(":");
+        return Integer.parseInt(token[0]) * 3600 + Integer.parseInt(token[1]) * 60 + Integer.parseInt(token[2]);
+    }
+}
