c102a032d03c5f1e686dcd35ba13293123dfc80e
방은혁 <bahngeh@naver.com>
2022-03-16 11:21:37 +0900

    Programmers 징검다리 건너기 다른 풀이
    윈도우를 활용한 풀이

--- 프로그래머스/Level3/징검다리 넌너기.java
+++ 프로그래머스/Level3/징검다리 넌너기.java
@@ -40,3 +40,46 @@ class Solution {
         return maxEmptyLen < k;
     }
 }
+
+//윈도우를 활용한 풀이
+import java.util.*;
+
+class Solution {
+    public int solution(int[] stones, int k) {
+        int answer = Integer.MAX_VALUE;
+        
+        Deque<Stone> window = new ArrayDeque<>();
+        for(int i = 0; i < stones.length; ++i) {
+            Stone stone = new Stone(i, stones[i]);
+            
+            //window의 스톤 개수가 k가 넘을 경우
+            if(!window.isEmpty() && window.getFirst().idx <= i - k) {
+                window.poll();
+            }
+            
+            //window에서 자기보다 작은 값은 삭제
+            while(!window.isEmpty() && window.getLast().val <= stone.val) {
+                window.removeLast();
+            }
+            
+            window.addLast(stone);
+            
+            //해당 윈도우에서 최대값과 정답을 비교
+            if(answer > window.getFirst().val && i >= k - 1) {
+                answer = window.getFirst().val;
+            }
+        }
+        
+        return answer;
+    }
+    
+    class Stone {
+        int idx;
+        int val;
+        
+        Stone (int idx, int val) {
+            this.idx = idx;
+            this.val = val;
+        }
+    }
+}
