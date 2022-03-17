706475360ad5086653540ef24f72e3d60168e0b7
방은혁 <bahngeh@naver.com>
2022-03-17 11:52:56 +0900

    Programmers 모두 0으로 만들기 다른 풀이
    그래프를 위상정렬을 통해 분산 순서를 정해 원하는 답을 구한 풀이.

--- 프로그래머스/Level3/모두 0으로 만들기.java
+++ 프로그래머스/Level3/모두 0으로 만들기.java
@@ -76,3 +76,99 @@ class Solution {
         vertexW[v] = 0;
     }
 }
+
+//위상 정렬 이용
+import java.util.*;
+
+class Solution {
+    
+    ArrayList<Integer>[] graph;
+    int[] edgeCnt;
+    long[] weights;
+    
+    public long solution(int[] a, int[][] edges) {
+        
+        //가중치들을 0으로 만들 수 없는 경우
+        if(Arrays.stream(a).sum() != 0) {
+            return -1;
+        }
+        
+        graph = makeGraph(a.length, edges);
+        edgeCnt = init(); //위상정렬을 위한 정보
+        weights = intToLong(a);
+        
+        //위상정렬 순서대로 가중치 분산
+        return topologicalSort();
+    }
+    
+    ArrayList<Integer>[] makeGraph(int n, int[][] edges) {
+        ArrayList<Integer>[] graph = new ArrayList[n];
+        for(int i = 0; i < n; ++i) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        int u, v;
+        for(int i = 0; i < edges.length; ++i) {
+            u = edges[i][0];
+            v = edges[i][1];
+            
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+        return graph;
+    }
+    
+    long[] intToLong(int[] a) {
+        long[] weights = new long[a.length];
+        for(int i = 0; i < a.length; ++i) {
+            weights[i] = a[i];
+        }
+        return weights;
+    }
+    
+    int[] init() {
+        int[] edgeCnt = new int[graph.length];
+        for(int i = 0; i < graph.length; ++i) {
+            edgeCnt[i] = graph[i].size();
+        }
+        return edgeCnt;
+    }
+    
+    long topologicalSort() {
+        
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] isVisited = new boolean[graph.length];
+        long moveCnt = 0;
+        
+        for(int i = 0; i < edgeCnt.length; ++i) {
+            if(edgeCnt[i] == 1) {
+                q.add(i);
+            }
+        }
+        
+        while(!q.isEmpty()) {
+            int from = q.poll();
+            isVisited[from] = true;
+            
+            for(int i = 0; i < graph[from].size(); ++i) {
+                int to = graph[from].get(i);
+                if(isVisited[to]) {
+                    continue;
+                }
+                
+                //가중치 분산
+                edgeCnt[to]--;
+                weights[to] += weights[from];
+                moveCnt += Math.abs(weights[from]);
+                weights[from] = 0;
+                
+                if(edgeCnt[to] == 1) {
+                    q.add(to);
+                }
+            }
+        }
+        
+        return moveCnt;
+    }
+    
+}
