b29289fc4b172aeaeab11e77183ee32f248e839f
방은혁 <bahngeh@naver.com>
2022-03-18 12:14:24 +0900

    BOJ 1662(압축) 다른 풀이
    재귀함수 이용해 압축 풀기

--- 백준/재귀/1662(압축).java
+++ 백준/재귀/1662(압축).java
@@ -47,3 +47,46 @@ public class Main {
         str = br.readLine();
     }
 }
+
+//재귀 풀이
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static String str;
+    static boolean[] visit;
+
+    public static void main(String[] args) throws IOException {
+        input();
+
+        visit = new boolean[str.length()];
+        System.out.println(dfs(0));
+    }
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        str = br.readLine();
+    }
+
+    static int dfs(int start) {
+        int len = 0;
+
+        for(int i = start; i < str.length(); ++i) {
+            if(visit[i]) {
+                continue;
+            }
+
+            visit[i] = true;
+            if(str.charAt(i) == '(') {
+                len--;
+                len += (str.charAt(i - 1) - '0') * dfs(i + 1);
+            } else if(str.charAt(i) == ')') {
+                return len;
+            } else {
+                len++;
+            }
+        }
+        return len;
+    }
+}
