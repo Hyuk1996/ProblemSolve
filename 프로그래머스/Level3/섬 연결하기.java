c6d9ced08dc39a7ca7d75a25824c0cf9d62ea4c2
방은혁 <bahngeh@naver.com>
2022-03-18 10:34:42 +0900

    Programmers 섬 연결하기 다른 풀이
    크루스칼 알고리즘을 이용해 MST 구하기

--- 프로그래머스/Level3/섬 연결하기.java
+++ 프로그래머스/Level3/섬 연결하기.java
@@ -71,3 +71,66 @@ class Solution {
         }
     }
 }
+
+//Kruskal 풀이
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[][] costs) {
+        
+        //간선 배열을 가중치 기준으로 정렬
+        Arrays.sort(costs, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[2] - o2[2];
+            }
+        });
+        
+        DisjointSet set = new DisjointSet(n);
+        int totalWeight = 0;
+        int edgeCnt = 0;
+        int u, v, w;
+        for(int i = 0; i < costs.length; ++i) {
+            u = costs[i][0];
+            v = costs[i][1];
+            w = costs[i][2];
+            
+            if(set.find(u) == set.find(v)) {
+                continue;
+            }
+            set.merge(u, v);
+            edgeCnt++;
+            totalWeight += w;
+        }
+        
+        return totalWeight;
+    }
+    
+    class DisjointSet {
+        int[] parent;
+        
+        DisjointSet(int n) {
+            parent = new int[n];
+            for(int i = 0; i < n; ++i) {
+                parent[i] = i;
+            }
+        }
+        
+        int find(int v) {
+            if(parent[v] == v) {
+                return v;
+            }
+            return parent[v] = find(parent[v]); //경로압축
+        }
+        
+        void merge(int u, int v) {
+            int rootU = find(u);
+            int rootV = find(v);
+            
+            if(rootU == rootV) {
+                return;
+            }
+            parent[rootV] = rootU;
+        }
+    }
+}
