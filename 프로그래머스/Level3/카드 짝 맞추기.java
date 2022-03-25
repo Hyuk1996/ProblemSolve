802cf1e5252a180b8d94b83efc82e9705daab686
방은혁 <bahngeh@naver.com>
2022-03-25 18:16:53 +0900

    Programmers 카드 짝 맞추기 ver2
    다시 풀어봄

--- 프로그래머스/Level3/카드 짝 맞추기.java
+++ 프로그래머스/Level3/카드 짝 맞추기.java
@@ -2,186 +2,193 @@ import java.util.*;
 
 class Solution {
     
-    Map<Integer, List<Pos>> map;
-    List<Integer> cards;
-    List<List<Integer>> permutations = new ArrayList<>();
-
+    final int BOARD_SIZE = 4;
+    
+    Map<Integer, List<Point>> map;
+    List<List<Integer>> permutations;
+    int answer = Integer.MAX_VALUE;
+    
     final int[] dr = {-1, 1, 0, 0};
     final int[] dc = {0, 0, -1, 1};
     
-    int answer = Integer.MAX_VALUE;
-    
     public int solution(int[][] board, int r, int c) {
         
-        //보드에서 카드 번호와 좌표정보 구해서 map에 저장
+        //board에서 카드 정보를 구하기
         map = getInfo(board);
         
-        //카드를 지울 수 있는 모든 순서 구하기(순열)
-        cards = new ArrayList<>(map.keySet());
-        boolean[] isVisited = new boolean[cards.size()];
+        //카드를 뽑을 수 있는 모든 순열 구하기
+        permutations = new ArrayList<>();
+        List<Integer> cards = new ArrayList(map.keySet());
+        boolean[] isSelected = new boolean[cards.size()];
         Integer[] permutation = new Integer[cards.size()];
         
-        getPermutation(isVisited, permutation, 0);
-    
-        //가능한 모든 순서에 대해 게임 진행하기
-        for(List<Integer> permu : permutations) {
-            gameStart(permu, board, r, c, 0, 0);
+        getPermutations(cards, isSelected, permutation, 0);
+        
+        
+        //각 순서에 대해 게임 진행
+        for(List<Integer> cardOrder : permutations) {
+            game(cardOrder, board, r, c, 0, 0);
         }
         
         return answer;
     }
     
-    Map<Integer, List<Pos>> getInfo(int[][] board) {
-        Map<Integer, List<Pos>> map = new HashMap<>();
-        
-        for(int i = 0; i < board.length; ++i) {
-            for(int j = 0; j < board.length; ++j) {
+    Map<Integer, List<Point>> getInfo(int[][] board) {
+        Map<Integer, List<Point>> map = new HashMap<>();
+        for(int i = 0; i < BOARD_SIZE; ++i) {
+            for(int j = 0; j < BOARD_SIZE; ++j) {
                 if(board[i][j] == 0) {
                     continue;
                 }
-                if(map.containsKey(board[i][j])) {
-                    map.get(board[i][j]).add(new Pos(i, j));
+                
+                int cardNum = board[i][j];
+                if(map.containsKey(cardNum)) {
+                    map.get(cardNum).add(new Point(i, j));
                 } else {
-                    List<Pos> pos = new ArrayList<>();
-                    pos.add(new Pos(i, j));
-                    map.put(board[i][j], pos);
+                    List<Point> list = new ArrayList<>();
+                    list.add(new Point(i, j));
+                    map.put(cardNum, list);
                 }
             }
         }
-        
         return map;
     }
     
-    void getPermutation(boolean[] isVisited, Integer[] permutation, int depth) {
+    void getPermutations(List<Integer> cards, boolean[] isSelected, Integer[] permutation, int depth) {
         if(depth == cards.size()) {
             permutations.add(new ArrayList<>(Arrays.asList(permutation)));
+            return;
         }
         
         for(int i = 0; i < cards.size(); ++i) {
-            if(!isVisited[i]) {
-                isVisited[i] = true;
+            if(!isSelected[i]) {
+                isSelected[i] = true;
                 permutation[depth] = cards.get(i);
-                getPermutation(isVisited, permutation, depth + 1);
-                isVisited[i] = false;
+                getPermutations(cards, isSelected, permutation, depth + 1);
+                isSelected[i] = false;
             }
         }
     }
     
-    void gameStart(List<Integer> permu, int[][] board, int r, int c, int depth, int moveCnt) {
-        if(depth == permu.size()) {
+    void game(List<Integer> cardOrder, int[][] board, int r, int c, int gameTurn, int moveCnt) {
+        if(gameTurn == cardOrder.size()) {
             answer = Math.min(answer, moveCnt);
             return;
         }
         
-        int r1 = map.get(permu.get(depth)).get(0).r;
-        int c1 = map.get(permu.get(depth)).get(0).c;
+        int cardNum = cardOrder.get(gameTurn);
+        int r1 = map.get(cardNum).get(0).r;
+        int c1 = map.get(cardNum).get(0).c;
+        int r2 = map.get(cardNum).get(1).r;
+        int c2 = map.get(cardNum).get(1).c;
         
-        int r2 = map.get(permu.get(depth)).get(1).r;
-        int c2 = map.get(permu.get(depth)).get(1).c;
-        
-        //하나의 카드를 없애는 방법도 2가지 경우의 수 존재
-        //지우는 순서 (r1, c1) -> (r2, c2)
+        //(r1, c1) -> (r2, c2)
         int[][] tmp1 = copyBoard(board);
-        int moveCnt1 = moveCnt;
+        int tmpCnt1 = moveCnt;
         
-        moveCnt1 += move(tmp1, r, c, r1, c1);
-        moveCnt1 += move(tmp1, r1, c1, r2, c2);
+        tmpCnt1 += move(tmp1, r, c, r1, c1);
+        tmpCnt1 += move(tmp1, r1, c1, r2, c2);
+        tmpCnt1 += 2; //Enter
+        
         tmp1[r1][c1] = 0;
         tmp1[r2][c2] = 0;
-        gameStart(permu, tmp1, r2, c2, depth + 1, moveCnt1);
+        game(cardOrder, tmp1, r2, c2, gameTurn + 1, tmpCnt1);
         
-        //지우는 순서 (r2, c2) -> (r1, c1)
+        //(r2, c2) -> (r1, c1)
         int[][] tmp2 = copyBoard(board);
-        int moveCnt2 = moveCnt;
+        int tmpCnt2 = moveCnt;
         
-        moveCnt2 += move(tmp2, r, c, r2, c2);
-        moveCnt2 += move(tmp2, r2, c2, r1, c1);
-        tmp2[r2][c2] = 0;
+        tmpCnt2 += move(tmp2, r, c, r2, c2);
+        tmpCnt2 += move(tmp2, r2, c2, r1, c1);
+        tmpCnt2 += 2; //Enter
+        
         tmp2[r1][c1] = 0;
-        gameStart(permu, tmp2, r1, c1, depth + 1, moveCnt2);
-    }
-    
-    int move(int[][] board, int fromR, int fromC, int toR, int toC) {
-        Queue<Pos> q = new LinkedList<>();
-        int[][] dist = new int[board.length][board.length];
-        init(dist);
-        
-        dist[fromR][fromC] = 0;
-        q.add(new Pos(fromR, fromC));
-        while(!q.isEmpty()) {
-            Pos pos = q.poll();
-            
-            if(pos.r == toR && pos.c == toC) {
-                return dist[toR][toC] + 1;
-            }
-            
-            //방향키만 이동
-            for(int i = 0; i < 4; ++i) {
-                int nextR = pos.r + dr[i];
-                int nextC = pos.c + dc[i];
-                
-                if(nextR < 0 || nextR >= 4 || nextC < 0 || nextC >= 4) {
-                    continue;
-                }
-                if(dist[nextR][nextC] < dist[pos.r][pos.c] + 1) {
-                    continue;
-                }
-                dist[nextR][nextC] = dist[pos.r][pos.c] + 1;
-                q.add(new Pos(nextR, nextC));
-            }
-            
-            //컨트롤 + 방향키
-            for(int i = 0; i < 4; ++i) {
-                int nextR = pos.r + dr[i];
-                int nextC = pos.c + dc[i];
-                
-                if(nextR < 0 || nextR >= 4 || nextC < 0 || nextC >= 4) {
-                    continue;
-                }
-                while(true) {
-                    if(board[nextR][nextC] != 0) {
-                        break;
-                    }
-                    nextR = nextR + dr[i];
-                    nextC = nextC + dc[i];
-                    if(nextR < 0 || nextR >= 4 || nextC < 0 || nextC >= 4) {
-                        nextR -= dr[i];
-                        nextC -= dc[i];
-                        break;
-                    }
-                }
-                
-                if(dist[nextR][nextC] < dist[pos.r][pos.c] + 1) {
-                    continue;
-                }
-                dist[nextR][nextC] = dist[pos.r][pos.c] + 1;
-                q.add(new Pos(nextR, nextC));
-            }
-        }
-        return -1;
-    }
-    
-    void init(int[][] dist) {
-        for(int i = 0; i < dist.length; ++i) {
-            for(int j = 0; j < dist.length; ++j) {
-                dist[i][j] = Integer.MAX_VALUE;
-            }
-        }
+        tmp2[r2][c2] = 0;
+        game(cardOrder, tmp2, r1, c1, gameTurn + 1, tmpCnt2);
     }
     
     int[][] copyBoard(int[][] board) {
-        int[][] tmp = new int[board.length][board.length];
-        for(int i = 0; i < board.length; ++i) {
-            for(int j = 0; j < board.length; ++j) {
+        int[][] tmp = new int[BOARD_SIZE][BOARD_SIZE];
+        for(int i = 0; i < BOARD_SIZE; ++i) {
+            for(int j = 0; j < BOARD_SIZE; ++j) {
                 tmp[i][j] = board[i][j];
             }
         }
         return tmp;
     }
     
-    class Pos {
+    int move(int[][] board, int startR, int startC, int endR, int endC) {
+        Queue<Point> q = new LinkedList<>();
+        int[][] dist = init();
+        
+        dist[startR][startC] = 0;
+        q.add(new Point(startR, startC));
+        while(!q.isEmpty()) {
+            Point p = q.poll();
+            
+            if(p.r == endR && p.c == endC) {
+                return dist[endR][endC];
+            }
+            
+            //방향키
+            for(int i = 0; i < 4; ++i) {
+                int nextR = p.r + dr[i];
+                int nextC = p.c + dc[i];
+                
+                if(nextR < 0 || nextR >= BOARD_SIZE || nextC < 0 || nextC >= BOARD_SIZE) {
+                    continue;
+                }
+                if(dist[nextR][nextC] < dist[p.r][p.c] + 1) {
+                    continue;
+                }
+                dist[nextR][nextC] = dist[p.r][p.c] + 1;
+                q.add(new Point(nextR, nextC));
+            }
+            
+            //Ctrl+방향키
+            for(int i = 0; i < 4; ++i) {
+                int nextR = p.r + dr[i];
+                int nextC = p.c + dc[i];
+                
+                if(nextR < 0 || nextR >= BOARD_SIZE || nextC < 0 || nextC >= BOARD_SIZE) {
+                    continue;
+                }
+                
+                while(true) {
+                    if(board[nextR][nextC] != 0) {
+                        break;
+                    }
+                    
+                    nextR += dr[i];
+                    nextC += dc[i];
+                    if(nextR < 0 || nextR >= BOARD_SIZE || nextC < 0 || nextC >= BOARD_SIZE) {
+                        nextR -= dr[i];
+                        nextC -= dc[i];
+                        break;
+                    }
+                }
+                
+                if(dist[nextR][nextC] < dist[p.r][p.c] + 1) {
+                    continue;
+                }
+                dist[nextR][nextC] = dist[p.r][p.c] + 1;
+                q.add(new Point(nextR, nextC));
+            }
+        }
+        return -1;
+    }
+    
+    int[][] init() {
+        int [][] dist = new int[BOARD_SIZE][BOARD_SIZE];
+        for(int i = 0; i < BOARD_SIZE; ++i) {
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
+        }
+        return dist;
+    }
+ 
+    class Point {
         int r, c;
-        Pos(int r, int c) {
+        Point(int r, int c) {
             this.r = r;
             this.c = c;
         }
