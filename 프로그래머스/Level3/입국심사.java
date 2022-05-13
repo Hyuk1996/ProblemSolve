e21654287e5e8c79277b97c7edc1e95723cc3cd0
방은혁 <bahngeh@naver.com>
2022-05-14 00:50:47 +0900

    Programmers 입국심사 다른 풀이
    재귀 함수 이용 이진탐색

--- 프로그래머스/Level3/입국심사.java
+++ 프로그래머스/Level3/입국심사.java
@@ -37,3 +37,42 @@ class Solution {
         return cnt >= n;
     }
 }
+
+//재귀 풀이
+class Solution {
+
+    private int n;
+    private int[] times;
+
+    public long solution(int n, int[] times) {
+
+        long min = 0;
+        long max = (long)(1e9) * n;
+
+        this.n = n;
+        this.times = times;
+
+        return binarySearch(min, max);
+    }
+
+    private long binarySearch(long lo, long hi) {
+        if(lo + 1 == hi) {
+            return hi;
+        }
+        
+        long mid = lo + (hi - lo) / 2;
+        if(possible(mid)) {
+            return binarySearch(lo, mid);
+        } else {
+            return binarySearch(mid, hi);
+        }
+    }
+
+    private boolean possible(long hour) {
+        long cnt = 0;
+        for(int time : times) {
+            cnt += hour / time;
+        }
+        return cnt >= n;
+    }
+}
