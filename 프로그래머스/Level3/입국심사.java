72afe93e0ee523da4d78ddd88120acd8644bcce0
방은혁 <bahngeh@naver.com>
2022-05-14 00:37:07 +0900

    Programmers 입국심사 풀이 v2
    이분탐색 문제

--- 프로그래머스/Level3/입국심사.java
+++ 프로그래머스/Level3/입국심사.java
@@ -1,41 +1,39 @@
 class Solution {
-    public long solution(int n, int[] times) {
-        long answer = 0;
-        
-        long maxTime = getMaxTime(times);
-        
-        //총 걸리는 심사시간을 기준으로 이분탐색을 통해 최소 시간 찾기.
-        long leftT = 0;
-        long rightT = maxTime * n;
-        long midT;
-        while(leftT + 1 < rightT) {
-            midT = leftT + (rightT - leftT) / 2;
-            //현재 추측 시간에 검사받을 수 있는 사람 수 계산하기.
-            long cnt = calCnt(times, midT);
 
-            if(cnt < n) {
-                leftT = midT;
+    private int n;
+    private int[] times;
+
+    public long solution(int n, int[] times) {
+
+        long min = 1;
+        long max = (long)(1e9) * n;
+
+        this.n = n;
+        this.times = times;
+
+        return binarySearch(min, max);
+    }
+
+    private long binarySearch(long min, long max) {
+        long lo = 0;
+        long hi = max;
+        long mid;
+        while(lo + 1 < hi) {
+            mid = lo + (hi - lo) / 2;
+            if(possible(mid)) {
+                hi = mid;
             } else {
-                rightT = midT;
+                lo = mid;
             }
         }
-        answer = rightT;
-        return answer;
+        return hi;
     }
-    
-    int getMaxTime(int[] times) {
-        int maxTime = 0;
-        for(int i = 0; i < times.length; ++i) {
-            maxTime = Math.max(maxTime, times[i]);
-        }
-        return maxTime;
-    }
-    
-    long calCnt(int[] times, long midT) {
+
+    private boolean possible(long hour) {
         long cnt = 0;
-        for(int i = 0; i < times.length; ++i) {
-            cnt += midT / times[i];
+        for(int time : times) {
+            cnt += hour / time;
         }
-        return cnt;
+        return cnt >= n;
     }
 }
