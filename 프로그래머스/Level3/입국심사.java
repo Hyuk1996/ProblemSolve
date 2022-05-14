e615344bb99892f3bb4b1800770b0e328f632e03
방은혁 <bahngeh@naver.com>
2022-05-14 12:03:54 +0900

    Programmers 입국심사 다른 풀이
    우선순위 큐와 최적해 이용해 원하는 답 구하기

--- 프로그래머스/Level3/입국심사.java
+++ 프로그래머스/Level3/입국심사.java
@@ -76,3 +76,43 @@ class Solution {
         return cnt >= n;
     }
 }
+
+//우선수위 큐 & 최적해 이용한 풀이
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class Solution {
+
+    public long solution(int n, int[] times) {
+
+        //심사관이 동시에 심사를 진행할 때, 분당 처리할 수 있는 손님 수 구하기
+        double vel = 0;
+        for (int t : times) {
+            vel += 1D / t;
+        }
+
+        //최적의 심사시간 구하기
+        double optimalTime = n / vel;
+
+        System.out.println(optimalTime);
+
+        //우선순위 큐 이용 실제 가능시간 구하기
+        PriorityQueue<long[]> pq = new PriorityQueue<>(
+            Comparator.comparingLong(arr -> arr[0] + arr[1]));
+
+        for(int t : times) {
+            long cnt = (long)optimalTime / t;
+            pq.add(new long[]{t, cnt * t});
+            n -= cnt;
+        }
+
+        long answer = (long)optimalTime;
+        for(int i = 0; i < n; ++i) {
+            long[] a = pq.poll();
+            a[1] += a[0];
+            pq.offer(a);
+            answer = a[1];
+        }
+        return answer;
+    }
+}
