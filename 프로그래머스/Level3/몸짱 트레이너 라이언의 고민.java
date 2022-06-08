3d5e3b35bc880950c4f70d875c9d6660b641edc1
방은혁 <bahngeh@naver.com>
2022-06-08 21:58:58 +0900

    Programmers 몸짱 트레이너 라이언의 고민 풀이 최적화
    최대로 겹치는 손님 수 구하는 알고리즘 최적화

--- 프로그래머스/Level3/몸짱 트레이너 라이언의 고민.java
+++ 프로그래머스/Level3/몸짱 트레이너 라이언의 고민.java
@@ -4,41 +4,37 @@ import java.util.List;
 class Solution {
 
     private static final int LAST_TIME = 1320;
+    private static final int START_TIME = 600;
 
     public int solution(int n, int m, int[][] timetable) {
 
         int maxConcurrentCustomers = getMaxConcurrentCustomers(timetable);
-
         if (maxConcurrentCustomers == 1) {
             return 0;
         }
-        
+
         return getMaxDistance(n, m, maxConcurrentCustomers);
     }
 
     private int getMaxConcurrentCustomers(int[][] timetable) {
-        int[] cnt = new int[LAST_TIME + 1];
+        int[] cnt = new int[LAST_TIME + 2];
         for (int[] time : timetable) {
-            int start = time[0];
-            int end = time[1];
-
-            for (int i = start; i <= end; ++i) {
-                cnt[i]++;
-            }
+            cnt[time[0]]++;
+            cnt[time[1] + 1]--;
         }
 
-        int maxConcurrentCustomer = 0;
-        for (int i = 0; i < cnt.length; ++i) {
-            if (maxConcurrentCustomer < cnt[i]) {
-                maxConcurrentCustomer = cnt[i];
-            }
+        int maxConcurrentCustomers = 0;
+        int curConcurrentCustomers = 0;
+        for (int i = START_TIME; i <= LAST_TIME; ++i) {
+            curConcurrentCustomers += cnt[i];
+            maxConcurrentCustomers = Math.max(maxConcurrentCustomers, curConcurrentCustomers);
         }
-        return maxConcurrentCustomer;
+        return maxConcurrentCustomers;
     }
 
     private int getMaxDistance(int n, int m, int maxConcurrentCustomers) {
         for (int dist = 2 * n - 2; dist > 0; --dist) {
-            for (int i = 0; i < n; ++i) {
+            for (int i = 0; i <= n / 2; ++i) {
                 List<Pos> customers = new ArrayList<>();
                 for (int y = 0; y < n; ++y) {
                     for (int x = 0; x < n; ++x) {
