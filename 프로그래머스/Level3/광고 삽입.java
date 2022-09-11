3a861abce9ff28dfce7fcd5c69717ff237f0c950
방은혁 <bahngeh@naver.com>
2022-09-11 21:06:15 +0900

    programmers 광고 삽입 풀이 ver2
    누적합 + 구현

--- 프로그래머스/Level3/광고 삽입.java
+++ 프로그래머스/Level3/광고 삽입.java
@@ -1,98 +1,72 @@
 class Solution {
-    
     public String solution(String play_time, String adv_time, String[] logs) {
         
-        int playTime = convertTime(play_time);
-        int[] video = new int[playTime + 1];
+        //비디오 누적 재생 횟수 구하기
+        int[] playCntRecord = getPlayCntRecord(play_time, logs);
         
-        //log를 이용해 video에 해당 시간에 시청자 시청자 수 기록
-        for(String log : logs) {
-            String[] times = log.split("-");
-            int startT = convertTime(times[0]);
-            int endT = convertTime(times[1]);
+        //광고 삽입 시간 구하기
+        int advStartTime = 0;
+        int advTime = convertTime(adv_time);
+        long maxWatchTime = 0;
+        for (int i = advStartTime; i < advTime; ++i) {
+            maxWatchTime += playCntRecord[i];
+        }
+        
+        long watchTime = maxWatchTime;
+        for (int i = 1; i + advTime <= convertTime(play_time); ++i) {
+            watchTime -= playCntRecord[i - 1];
+            watchTime += playCntRecord[i + advTime - 1];
             
-            for(int i = startT; i < endT; ++i) {
-                video[i] += 1;
+            if (watchTime > maxWatchTime) {
+                advStartTime = i;
+                maxWatchTime = watchTime;
             }
         }
         
-        //video 이용해 광고가 들어갈 위치 찾기
-        int advT = convertTime(adv_time);
-        int advStartT = 0;
-        long playSum = 0;
-        for(int i = 0; i < advT; ++i) {
-            playSum += video[i];
-        }
+        return convertTime(advStartTime);
+    }
+    
+    private int[] getPlayCntRecord(String playTime, String[] records) {
         
-        long maxPlaySum = playSum;
-        for(int i = advStartT + 1; i <= playTime - advT; ++i) {
-            playSum -= video[i - 1];
-            playSum += video[i + advT - 1];
+        int[] playCntRecord = new int[convertTime(playTime) + 1];
+        
+        for (String record : records) {
+            String[] times = record.split("-");
+            int startTime = convertTime(times[0]);
+            int endTime = convertTime(times[1]);
             
-            if(playSum > maxPlaySum) {
-                maxPlaySum = playSum;
-                advStartT = i;
-            }
+            playCntRecord[startTime]++;
+            playCntRecord[endTime]--;
         }
         
-        String answer = String.format("%02d:%02d:%02d", advStartT / 3600, (advStartT % 3600) / 60, (advStartT % 60)); 
-        return answer;
+        for (int i = 1; i < playCntRecord.length; ++i) {
+            playCntRecord[i] += playCntRecord[i - 1];
+        }
+        
+        return playCntRecord;
     }
     
-    int convertTime(String time) {
-        String[] token = time.split(":");
+    private int convertTime(String time) {
+        String[] tokens = time.split(":");
         
-        int convertTime = Integer.parseInt(token[0]) * 3600;
-        convertTime += Integer.parseInt(token[1]) * 60;
-        convertTime += Integer.parseInt(token[2]);
-        return convertTime;
+        int intTime = Integer.valueOf(tokens[2]);
+        intTime += Integer.valueOf(tokens[1]) * 60;
+        intTime += Integer.valueOf(tokens[0]) * 60 * 60;
+        
+        return intTime;
     }
     
-}
-
-//최적화 풀이
-class Solution {
-    public String solution(String play_time, String adv_time, String[] logs) {
+    private String convertTime(int time) {
+        StringBuilder stringTime = new StringBuilder();
         
-        int playT = convertToInt(play_time);
-        int[] viewers = new int[playT + 1];
+        stringTime.append(String.format("%02d:", time / (60 * 60)));
         
-        for(String log : logs) {
-            String[] times = log.split("-");
-            viewers[convertToInt(times[0])]++; //시작 시간 시청자 증가
-            viewers[convertToInt(times[1])]--; //끝 시간 시청자 감소
-        }
+        time = time % (60 * 60);
+        stringTime.append(String.format("%02d:", time / 60));
         
-        //각 시간에 시청자 수 구하기
-        for(int i = 1; i <= playT; ++i) {
-            viewers[i] += viewers[i - 1];
-        }
+        time = time % 60;
+        stringTime.append(String.format("%02d", time));
         
-        
-        //누적합 이용해 알맞은 광고 시작시간 구하기
-        int advT = convertToInt(adv_time);
-        long viewerCnt = 0;
-        for(int i = 0; i < advT; ++i) {
-            viewerCnt += viewers[i];
-        }
-        
-        long maxViewerCnt = viewerCnt;
-        int advStartT = 0;
-        for(int i = 1; i <= playT - advT; ++i) {
-            viewerCnt -= viewers[i - 1];
-            viewerCnt += viewers[i + advT - 1];
-            
-            if(viewerCnt > maxViewerCnt) {
-                maxViewerCnt = viewerCnt;
-                advStartT = i;
-            }
-        }
-        
-        return String.format("%02d:%02d:%02d", (advStartT / 3600), (advStartT % 3600) / 60, advStartT % 60);
-    }
-    
-    int convertToInt(String time) {
-        String[] token = time.split(":");
-        return Integer.parseInt(token[0]) * 3600 + Integer.parseInt(token[1]) * 60 + Integer.parseInt(token[2]);
+        return stringTime.toString();
     }
 }
