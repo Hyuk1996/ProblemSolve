cd86180a3ff85336ba1897f68d18590ea7ebf060
방은혁 <bahngeh@naver.com>
2022-09-28 11:35:52 +0900

    programmers 구명보트 상수 시간에 가까운 풀이
    정렬하지 않고, 무게를 기준으로 투 포인터

--- 프로그래머스/Level2/구명보트.java
+++ 프로그래머스/Level2/구명보트.java
@@ -21,3 +21,80 @@ class Solution {
         return boatCnt;
     }
 }
+
+/* O(241) 풀이 (몸무게 범위) */
+class Solution {
+    public int solution(int[] people, int limit) {
+        
+        int[] weights = new int[241];
+        for (int weight : people) {
+            weights[weight]++;
+        }
+        
+        int answer = 0;
+        
+        /* 무조건 혼자 타야하는 사람 */
+        for (int i = limit; i > limit - 40; --i) {
+            answer += weights[i];
+            weights[i] = 0;
+        }
+        
+        /* 같이 탈 수 있으면 같이타고, 불가능하면 혼자 타기 */
+        int light = 40;
+        int heavy = limit - 40;
+        while (light < heavy) {
+            if (weights[light] == 0) {
+                light++;
+                continue;
+            }
+            
+            if (weights[heavy] == 0) {
+                heavy--;
+                continue;
+            }
+            
+            if (light + heavy > limit) {
+                answer += weights[heavy];
+                
+                weights[heavy] = 0;
+                
+                heavy--;
+                continue;
+            }
+            
+            if (weights[light] > weights[heavy]) {
+                answer += weights[heavy];
+                
+                weights[light] -= weights[heavy];
+                weights[heavy] = 0;
+                
+                heavy--;
+            } else if (weights[light] < weights[heavy]){
+                answer += weights[light];
+                
+                weights[heavy] -= weights[light];
+                weights[light] = 0;
+                
+                light++;
+            } else {
+                answer += weights[light];
+            
+                weights[light] = 0;
+                weights[heavy] = 0;
+                
+                light++;
+                heavy--;
+            }
+        }
+        
+        if (light == heavy) {
+            if (light * 2 > limit) {
+                answer += weights[light];
+            } else {
+                answer += (int)Math.ceil((double)weights[light] / 2);
+            }
+        }
+        
+        return answer;
+    }
+}
