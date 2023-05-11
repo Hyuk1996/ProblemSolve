83f31d07250a99c182405f0673311c6cbb9825ea
방은혁 <bahngeh@naver.com>
2023-05-11 12:05:37 +0900

    study : solve '풍선 터뜨리기' by another way
    It can be solved by Deque

--- 백준/구현/2346(풍선 터뜨리기).kt
+++ 백준/구현/2346(풍선 터뜨리기).kt
@@ -1,5 +1,6 @@
 import java.util.*
 
+// Solution 1 : Implementation
 fun main() {
     // input
     val scanner = Scanner(System.`in`)
@@ -55,3 +56,46 @@ fun main() {
         print("$num ")
     }
 }
+
+// Solution 2 : Deque
+fun main() {
+    // input
+    val scanner = Scanner(System.`in`)
+
+    val N = scanner.nextInt()
+    val deque = ArrayDeque<Paper>(N)
+    for (idx in 1..N) {
+        val value = scanner.nextInt()
+        deque.add(Paper(idx, value))
+    }
+
+    // solution
+    val answer = mutableListOf<Int>()
+    while (!deque.isEmpty()) {
+        val paper = deque.removeFirst()
+        answer.add(paper.no)
+
+        if (deque.isEmpty()) {
+            break
+        }
+
+        if (paper.value > 0) {
+            for (cnt in 1 until paper.value) {
+                val tempPaper = deque.removeFirst()
+                deque.addLast(tempPaper)
+            }
+        } else {
+            for (cnt in 1 ..abs(paper.value)) {
+                val tempPaper = deque.removeLast()
+                deque.addFirst(tempPaper)
+            }
+        }
+    }
+
+    // output
+    for (num in answer) {
+        print("$num ")
+    }
+}
+
+data class Paper(val no: Int, val value: Int)
