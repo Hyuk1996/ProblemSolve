1ecd87019655c86022f50a92e28f7cb35d214878
방은혁 <bahngeh@naver.com>
2023-06-28 13:43:19 +0900

    study : solve "fibonacci-number"

--- leetcode/easy/fibonacci-number.kt
+++ leetcode/easy/fibonacci-number.kt
@@ -16,3 +16,27 @@ class Solution {
         return c
     }
 }
+
+// Solution2 : top-down
+class Solution {
+    fun fib(n: Int): Int {
+        if (n < 2) {
+            return n
+        }
+
+        val dp = IntArray(n + 1) { - 1}
+        dp[0] = 0
+        dp[1] = 1
+        return fibonacci(n, dp)
+    }
+
+    private fun fibonacci(n: Int, dp: IntArray): Int {
+        if (dp[n - 2] == -1) {
+            dp[n - 2] = fibonacci(n - 2, dp)
+        }
+        if (dp[n - 1] == -1) {
+            dp[n - 1] = fibonacci(n - 1, dp)
+        }
+        return dp[n - 1] + dp[n - 2]
+    }
+}
