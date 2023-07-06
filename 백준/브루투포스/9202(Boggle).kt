18d6685848bb037cccab60e0529ff6d91b5fe24c
방은혁 <bahngeh@naver.com>
2023-07-06 23:25:23 +0900

    study : solve "Boggle"
    problem about trie, brute-force and dfs

--- 백준/브루투포스/9202(Boggle).kt
+++ 백준/브루투포스/9202(Boggle).kt
@@ -1,3 +1,4 @@
+// Solution 1 : brute force
 private val boggle = Array(4) { CharArray(4) }
 private val visited = Array(4) { BooleanArray(4) { false } }
 private val dr = arrayOf(-1, 0, 1, -1, 1, -1, 0, 1)
@@ -107,3 +108,127 @@ fun findWord(r: Int, c: Int, curWord: StringBuilder, word: String): Boolean {
     }
     return false
 }
+
+// Solution 2 : optimize solution 1 using trie
+private val trie = Trie()
+private val map = Array(4) { CharArray(4) { ' ' } }
+private val visited = Array(4) { BooleanArray(4) { false } }
+private val dr = arrayOf(-1, 0, 1, -1, 1, -1, 0, 1)
+private val dc = arrayOf(-1, -1, -1, 0, 0, 1, 1, 1)
+
+fun main() {
+    // store words in trie
+    val w = readln().toInt()
+    for (i in 1..w) {
+        trie.insert(readln())
+    }
+
+    readln()
+
+    // start boole game
+    var b = readln().toInt()
+    while (true) {
+        for (r in 0..3) {
+            val columns = readln()
+            for (c in 0..3) {
+                map[r][c] = columns[c]
+            }
+        }
+
+        val findWords = mutableSetOf<String>()
+        for (r in 0..3) {
+            for (c in 0..3) {
+                if (trie.root.childs.contains(map[r][c])) {
+                    // init
+                    for (r in 0..3) {
+                        for (c in 0..3) {
+                            visited[r][c] = false
+                        }
+                    }
+
+                    visited[r][c] = true
+                    val curWord = StringBuilder().append(map[r][c])
+                    val curNode = trie.root.childs[map[r][c]]!!
+                    dfs(r, c, curWord, curNode, findWords)
+                }
+            }
+        }
+
+        // calculate score
+        var maxScore = 0
+        var longestWord = ""
+        val findWordsCnt = findWords.size
+
+        for (findWord in findWords) {
+            maxScore += when (findWord.length) {
+                1, 2 -> 0
+                3, 4 -> 1
+                5 -> 2
+                6 -> 3
+                7 -> 5
+                8 -> 11
+                else -> throw IllegalArgumentException()
+            }
+
+            if (findWord.length > longestWord.length) {
+                longestWord = findWord
+            } else if (findWord.length == longestWord.length && findWord < longestWord) {
+                longestWord = findWord
+            }
+        }
+
+        // answer
+        println("$maxScore $longestWord $findWordsCnt")
+
+        if (--b == 0) {
+            break
+        } else {
+            readln()
+        }
+    }
+}
+
+fun dfs(r: Int, c: Int, curWord: StringBuilder, curNode: Node, findWords: MutableSet<String>) {
+    // base case
+    if (curWord.length == 8 ) {
+        return
+    }
+
+    for (d in 0..7) {
+        val nextR = r + dr[d]
+        val nextC = c + dc[d]
+
+        if (nextR in 0..3 && nextC in 0..3) {
+            if (!visited[nextR][nextC] && curNode.childs.contains(map[nextR][nextC])) {
+                visited[nextR][nextC] = true
+                curWord.append(map[nextR][nextC])
+
+                if (curNode.childs[map[nextR][nextC]]!!.isEnd) {
+                    findWords.add(curWord.toString())
+                }
+
+                dfs(nextR, nextC, curWord, curNode.childs[map[nextR][nextC]]!!, findWords)
+
+                visited[nextR][nextC] = false
+                curWord.deleteCharAt(curWord.length - 1)
+            }
+        }
+    }
+}
+
+class Trie {
+    val root = Node(key = ' ', isEnd = false)
+
+    fun insert(word: String) {
+        var curNode = root
+        for (char in word) {
+            if (!curNode.childs.contains(char)) {
+                curNode.childs[char] = Node(key = char, isEnd = false)
+            }
+            curNode = curNode.childs[char]!!
+        }
+        curNode.isEnd = true
+    }
+}
+
+data class Node(val key: Char, val childs: MutableMap<Char, Node> = mutableMapOf(), var isEnd: Boolean)
